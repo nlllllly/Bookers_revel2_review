dc47224f1daf311644af6948b61105412457e72d
vagrant <vagrant@localhost.localdomain>
2020-08-10 06:18:32 +0000

    [update]resub3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,15 @@
 class Book < ApplicationRecord
 
+    has_many :likes, dependent: :destroy
+    has_many :book_comments, dependent: :destroy
+
     belongs_to :user
 
     validates :title, presence: true
     validates :body, presence: true, length: {maximum:200}
+
+    def liked_by?(user)
+        likes.where(user_id: user.id).exists?
+      end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resources :book_comments, only:[:create, :destroy]
+    resource :likes, only:[:create, :destroy]
+  end
   resource :home, only: [:index] do
     resources :about, only: [:index]
   end
