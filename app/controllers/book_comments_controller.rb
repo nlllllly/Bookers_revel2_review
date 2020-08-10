10d2136e8dabb0b7deaafca00e722122d5358aa0
vagrant <vagrant@localhost.localdomain>
2020-08-10 07:39:41 +0000

    [update]resub6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,16 +2,17 @@ class BookCommentsController < ApplicationController
 
     def create
         @book = Book.find(params[:book_id])
-        book_comment = current_user.book_comments.new(book_comment_params)
-        book_comment.book_id = @book.id
-        book_comment.save
-        redirect_to request.referer
+        @book_comment = current_user.book_comments.new(book_comment_params)
+        @book_comment.book_id = @book.id
+        @book_comment.save
+        # redirect_to request.referer
         
     end
     
     def destroy
-        BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-        redirect_to request.referer
+        @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+        @book_comment.destroy
+        # redirect_to request.referer
     end
 
 
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,16 @@
 class LikesController < ApplicationController
 
     def create
-        book = Book.find(params[:book_id])
-        like = current_user.likes.new(book_id: book.id)
-        like.save
-        redirect_to request.referer
+        @book = Book.find(params[:book_id])
+        @like = current_user.likes.new(book_id: @book.id)
+        @like.save
 
     end
 
     def destroy
-        book = Book.find(params[:book_id])
-        like = current_user.likes.find_by(book_id: book.id)
-        like.destroy
-        redirect_to request.referer
+        @book = Book.find(params[:book_id])
+        @like = current_user.likes.find_by(book_id: @book.id)
+        @like.destroy
     end
 
 end
